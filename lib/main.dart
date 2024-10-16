731a10e3fe8bcb2edd4df29a230f31181f479d81
jodey <>
2024-10-16 10:27:33 +0700

    modul 1-2

---
+++ lib/main.dart
@@ -0,0 +1,86 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const Scaffold(
+        body: Center(
+          child: BiggerText(text: 'Hello, world!'),
+        ),
+      ),
+    );
+  }
+}
+
+class Heading extends StatelessWidget {
+  final String text;
+
+  const Heading({Key? key, required this.text}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(text,
+        style: const TextStyle(
+          fontSize: 24.0,
+          fontWeight: FontWeight.bold,
+        ));
+  }
+}
+
+// class ContohStateful extends StatefulWidget {
+//   final String parameterWidget;
+
+//   const ContohStateful({Key? key, required this.parameterWidget})
+//       : super(key: key);
+
+//   @override
+
+// }
+
+// class _ContohStatefulState extends State<ContohStateful> {
+//   String _dataState;
+
+//   @override
+//   Widget build(BuildContext context) {
+
+//   }
+// }
+
+class BiggerText extends StatefulWidget {
+  final String text;
+
+  const BiggerText({Key? key, required this.text}) : super(key: key);
+
+  @override
+  _BiggerTextState createState() => _BiggerTextState();
+}
+
+class _BiggerTextState extends State<BiggerText> {
+  double _textSize = 16.0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Text(widget.text, style: TextStyle(fontSize: _textSize)),
+        ElevatedButton(
+          child: const Text("Perbesar"),
+          onPressed: () {
+            setState(() {
+              _textSize = 36.0;
+            });
+          },
+        )
+      ],
+    );
+  }
+}
